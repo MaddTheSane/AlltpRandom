3ebfcc90c6e048b279519cfa916fc7cea95fc917
C.W. Betts <computers57@hotmail.com>
2018-03-12 14:31:32 -0600

    More work.

--- AlltpRandom/ViewController.cs
+++ AlltpRandom/ViewController.cs
@@ -2,12 +2,91 @@
 
 using AppKit;
 using Foundation;
+using AlttpRandomizer.IO;
+using AlttpRandomizer.Net;
 using AlttpRandomizer.Properties;
+using AlttpRandomizer.Random;
+using AlttpRandomizer.Rom;
 
 namespace AlltpRandom
 {
     public partial class ViewController : NSViewController
     {
+        #region Windows wrapping
+        private HeartBeepSpeed GetHeartBeepSpeed()
+        {
+            HeartBeepSpeed retVal;
+
+            if (Enum.TryParse(heartBeepPopUp.SelectedItem.Title, true, out retVal))
+            {
+                return retVal;
+            }
+
+            return HeartBeepSpeed.Normal;
+        }
+
+        private RandomizerDifficulty GetRandomizerDifficulty()
+        {
+            RandomizerDifficulty difficulty;
+
+            if (seedField.StringValue.ToUpper().Contains("C"))
+            {
+                difficultyPopUp.SelectItem("Casual");
+                seedField.StringValue = seedField.StringValue.ToUpper().Replace("C", "");
+                difficulty = RandomizerDifficulty.Casual;
+            }
+            else if (seedField.StringValue.ToUpper().Contains("G"))
+            {
+                difficultyPopUp.SelectItem("Glitched");
+                seedField.StringValue = seedField.StringValue.ToUpper().Replace("G", "");
+                difficulty = RandomizerDifficulty.Glitched;
+            }
+            else if (seedField.StringValue.ToUpper().Contains("NORAND"))
+            {
+                difficultyPopUp.SelectItem("No Randomization");
+                difficulty = RandomizerDifficulty.None;
+            }
+            else
+            {
+                switch (difficultyPopUp.SelectedItem.Title)
+                {
+                    case "Casual":
+                        difficulty = RandomizerDifficulty.Casual;
+                        break;
+                    case "Glitched":
+                        difficulty = RandomizerDifficulty.Glitched;
+                        break;
+                    default:
+                        return RandomizerDifficulty.None;
+                }
+            }
+
+            return difficulty;
+        }
+
+        private string filename {
+            get {
+                var dir = directoryField.StringValue;
+                var fName = fileNameField.StringValue;
+                var str = new NSString(dir);
+                str = str.AppendPathComponent(new NSString(fName));
+                return str.ToString();
+            }
+        }
+
+        private RandomizerOptions GetOptions()
+        {
+            return new RandomizerOptions
+            {
+                Filename = filename,
+                SramTrace = sramTraceCheck.State == NSCellStateValue.On,
+                ShowComplexity = complexityCheck.State == NSCellStateValue.On,
+                Difficulty = GetRandomizerDifficulty(),
+                HeartBeepSpeed = GetHeartBeepSpeed(),
+            };
+        }
+        #endregion
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -18,8 +97,10 @@ namespace AlltpRandom
 
             sramTraceCheck.State = Settings.Default.SramTrace ? NSCellStateValue.On : NSCellStateValue.Off;
             complexityCheck.State = Settings.Default.ShowComplexity ? NSCellStateValue.On : NSCellStateValue.Off;
+            fileNameField.StringValue = Settings.Default.OutputFile;
         }
 
+        #region IBActions
         [Export("changeFolder:")]
         void ChangeFolder(Foundation.NSObject sender)
         {
@@ -41,12 +122,30 @@ namespace AlltpRandom
         [Export("generateRandomizedROM:")]
         void GenerateRandomizedROM(Foundation.NSObject sender)
         {
-
+            if (sender is NSButton senderButton)
+            {
+                if (senderButton.Tag == 1)
+                {
+                    // TODO: Generate multiple ROMs.
+                    return;
+                }
+            }
+            // TODO: Generate single ROM.
         }
 
         [Export("getSpoiler:")]
         void GetSpoiler(Foundation.NSObject sender)
         {
+            var txt = seedField.StringValue;
+            if (txt.Length == 0)
+            {
+                var alert = new NSAlert();
+                alert.MessageText = "No Seed";
+                alert.InformativeText = "There is no seed specified in the seed field.\n\nThis is needed to ";
+
+                alert.BeginSheet(this.View.Window);
+                return;
+            }
 
         }
 
@@ -67,6 +166,7 @@ namespace AlltpRandom
         {
 
         }
+        #endregion
 
         public override NSObject RepresentedObject
         {
